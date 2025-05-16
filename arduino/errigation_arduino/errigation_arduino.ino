f5c0a482f1415c6d369f6c87b85667ef68fa8947
clementthom <clementthom17@gmail.com>
2025-05-16 18:42:59 +0200

    création du fichier Arduino

---
+++ arduino/errigation_arduino/errigation_arduino.ino
@@ -0,0 +1,13 @@
+#include <ArduinoJson.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(6, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(6, HIGH);
+}
